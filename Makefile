08c1da126df4d285131102e397b050093ff5abd9
Yejun Su <goofan.su@gmail.com>
2023-11-17 16:15:43 +0800

    support backup and restore to qrcode

--- Makefile
+++ Makefile
@@ -7,5 +7,14 @@ privkey:
 paperkey: privkey
 	paperkey --secret-key privkey.gpg --output paperkey.txt
 
-restore: pubkey
+qrcode: privkey
+	paperkey --secret-key privkey.gpg --output-type raw | base64 | qrencode -o qrcode.png
+
+restore-paperkey: pubkey
 	paperkey --pubring pubkey.asc.gpg --secrets paperkey.txt --output restored.gpg
+
+restore-qrcode: pubkey
+	zbarimg qrcode.png | \
+                cut -d':' -f2 | \
+                base64 --decode | \
+                paperkey --pubring pubkey.asc.gpg --output restored.gpg
--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A toolkit for backing up my GnuPG private key";
+  description = "My toolkit for backing up GnuPG private key";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
@@ -7,7 +7,8 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShells.default =
-          pkgs.mkShell { packages = with pkgs; [ gnupg qrencode paperkey ]; };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [ gnupg zbar qrencode paperkey ];
+        };
       });
 }
